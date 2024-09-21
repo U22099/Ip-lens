9b0d3344b329b815ffc4bd5ed25dd1f8e93630b1
Daniel <u22099dandev@gmail.com>
2024-09-21 11:55:01 +0000

    —-

--- main.js
+++ main.js
@@ -3,11 +3,8 @@ import Model from "./script/model/init.js";
 import View from "./script/view/init.js";
 import Controller from "./script/controller/init.js";
 
-console.log("hi")
 //Initialising the model, view and controller classes
 const model = new Model();
 const view = new View();
 const controller = new Controller(model, view);
-console.log("hey")
 controller.run();
-console.log("hello")
