97f4d4b06da72fde9ea2c1cb565e1de4028fec59
Daniel <u22099dandev@gmail.com>
2024-09-22 04:14:21 +0000

    —-

--- script/model/map.js
+++ script/model/map.js
@@ -1,21 +1,21 @@
-import Map from 'ol/Map.js';
-import OSM from 'ol/source/OSM.js';
-import TileLayer from 'ol/layer/Tile.js';
-import View from 'ol/View.js';
-import { fromLonLat } from 'ol/proj.js';
+import Map from "ol/Map.js";
+import OSM from "ol/source/OSM.js";
+import TileLayer from "ol/layer/Tile.js";
+import View from "ol/View.js";
+import { fromLonLat } from "ol/proj.js";
 
-export default function createMap(lat, long){
+export default function createMap(lat, long) {
   const map = new Map({
     layers: [
       new TileLayer({
         source: new OSM(),
       }),
     ],
-    target: 'map',
+    target: "map",
     view: new View({
       center: fromLonLat([long, lat]),
       zoom: 12,
     }),
   });
-  return map
+  return map;
 }
