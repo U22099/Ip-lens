6a3fca440a6c828bb67981a1d16aec10fffcafe2
Daniel <u22099dandev@gmail.com>
2024-09-23 17:35:21 +0000

    Adding PWA feature

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,45 @@
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   base: '/Ip-lens/',
+  plugins: [VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'IP Lens',
+        short_name: 'IP Lens',
+        description: 'Ip address tracker with map',
+        background_color: 'black', 
+        display: 'standalone', 
+        start_url: '/Ip-lens',
+        scope: '/',
+        orientation: 'portrait',
+        theme_color: 'purple',
+        icons: [
+		{
+			src: "pwa-64x64.png",
+			sizes: "64x64",
+			type: "image/png"
+		},
+		{
+			src: "pwa-192x192.png",
+			sizes: "192x192",
+			type: "image/png"
+		},
+  		{
+    			src: "pwa-512x512.png",
+    			sizes: "512x512",
+    			type: "image/png"
+  		},
+  		{
+    			src: "maskable-icon-512x512.png",
+    			sizes: "512x512",
+    			type: "image/png",
+    			purpose: "maskable"
+  		}
+	]
+      }
+  })],
   build: {
     sourcemap: true,
   },
