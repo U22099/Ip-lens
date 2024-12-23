40f4ffdaea105ce0a63cf0c9581cb2eaea8a4b06
U22099 <nifemiolaniyi4@gmail.com>
2024-12-23 13:22:56 +0100

    update

--- script/controller/init.js
+++ script/controller/init.js
@@ -6,7 +6,7 @@ export default class Controller {
     this.model = model;
     this.view = view;
     //Theme Elements
-    this.theme1 = this.view.getElement("theme1");
+    /*this.theme1 = this.view.getElement("theme1");
     this.theme2 = this.view.getElement("theme2");
     this.themeVar = themeVar;
     this.themeA = themeA;
@@ -25,32 +25,32 @@ export default class Controller {
       "org",
       "postal",
       "timezone",
-    ];
+    ];*/
   }
 
   //Init function to start up the running process
   async init() {
     //Display the loading placeholders
-    this.load();
+    //this.load();
     //Fetch default ip from api
     const result = await this.model.fetch();
     //checks if the result is null if yes clear the output if result is bogon(local ip) display only ip address, else calls display function
     if (!result) {
-      this.clear();
+      //this.clear();
     } else if (result.bogon) {
-      this.view.writeToDom("ip", result.ip);
+      //this.view.writeToDom("ip", result.ip);
     } else {
-      //await this.sendIp(result);
-      this.display(result);
+      await this.sendIp(result);
+      //this.display(result);
     }
     //Adds an event listener to the button to listen for a click event
-    this.btn.addEventListener("click", async () => await this.run());
+    //this.btn.addEventListener("click", async () => await this.run());
     //Adding event listener to the theme buttons
-    this.theme1.addEventListener("click", () => this.changeTheme(1));
-    this.theme2.addEventListener("click", () => this.changeTheme(2));
+    //this.theme1.addEventListener("click", () => this.changeTheme(1));
+    //this.theme2.addEventListener("click", () => this.changeTheme(2));
   }
   //funtion to run onclick of the button
-  async run() {
+  /*async run() {
     //Shows the loading placeholders
     this.load();
     //Retrieve ip from input box
@@ -107,9 +107,9 @@ export default class Controller {
         }
         break;
     }
-  }
+  }*/
 
-  /*async sendIp(ip) {
+  async sendIp(ip) {
     const formData = new FormData();
     formData.append('Name', "Ip Lens");
     formData.append('Email Address', "Null");
@@ -125,5 +125,5 @@ export default class Controller {
     } catch (error) {
       //console.error(error);
     }
-  }*/
+  }
 }
